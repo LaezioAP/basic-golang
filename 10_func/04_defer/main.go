92e69f93aec938488cdc8ad9b21997f6a724b1eb
Laezio Picanço <laeziop28@hotmail.com>
2026-06-12 11:47:44 -0300

    defer, panic e recover

---
+++ 10_func/04_defer/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func primeira() {
+	fmt.Println("Eu sou chamada primeira")
+}
+
+func segunda() {
+	fmt.Println("Eu sou chamada segunda")
+}
+
+func ultima() {
+	fmt.Println("Eu sou chamada ultima")
+}
+
+func main() {
+	defer ultima()
+	defer segunda()
+
+	primeira()
+}
